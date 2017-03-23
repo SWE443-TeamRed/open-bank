cd7039d033e63a0ce617675d2d65ed873a17ee8b
FA <fbagmu@gmail.com>
2017-03-23 10:42:12 -0400

    model created

---
+++ src/main/java/net/Red/atm/Model.java
@@ -0,0 +1,34 @@
+package net.Red.atm;
+
+import de.uniks.networkparser.graph.Association;
+import de.uniks.networkparser.graph.AssociationTypes;
+import de.uniks.networkparser.graph.Cardinality;
+import de.uniks.networkparser.graph.Clazz;
+import de.uniks.networkparser.graph.DataType;
+import org.sdmlib.models.classes.ClassModel;
+import org.sdmlib.storyboards.Storyboard;
+
+/**
+ * Created by FA on 3/23/2017.
+ */
+public class Model {
+
+    public static void main(String[] args) {
+        //create class model
+        ClassModel model = new ClassModel("net.Red.atm");
+
+
+        // create class user
+        Clazz user = model.createClazz("User");
+
+        // set attributes
+        user.withAttribute("name", DataType.STRING);
+        user.withAttribute("UserID",DataType.STRING);
+        //user.withAttribute("DOB",DataType.STRING);
+
+
+        model.generate();
+    }
+}
+
+
